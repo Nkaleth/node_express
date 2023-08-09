a653fed7066b44807543986deaf7a2e46bf36459
Nkaleth <nseguralu@gmail.com>
2023-08-09 16:27:40 -0500

    ADD views for main, about, home, 404, 500 and the main layout

--- meadowlark/meadowlark.js
+++ meadowlark/meadowlark.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const expressHandlebars = require('express-handlebars');
 
 const app = express();
+
+// configure Handlebars view engine
+app.engine('handlebars', expressHandlebars({
+  defaultLayout: 'main',
+}));
+
+app.set('view engine', 'handlebars');
 
 const port = process.env.PORT || 3000;
 
